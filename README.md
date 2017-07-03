7d03e65f65e3928bfb48f29c9c94047e1694d9e0
x70b1 <x70b1@users.noreply.github.com>
2017-07-03 20:50:43 +0200

    rewrite script

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # check_routeros-upgrade
-	
-This script checks if there is an upgrade for a mikrotik device available. It should work with different monitoring frameworks like Nagios, Icinga, Naemon, Shinken or Sensu.
+
+A simple monitoring plugin to check RouterOS for updates. A simple monitoring plugin to check RouterOS for updates.
 
 **Features**
-* check your device over SNMP or SSH
+* connect with SNMP or SSH
 * select your RouterOS release tree
 
 **Exampleoutput**
-* RouterOS version 6.38.3 is up to date (Release: Di 07 Feb 2017)
-* RouterOS is upgradable to version 6.39rc33 (Release: Fr 17 Feb 2017)
+* RouterOS 6.39.2 is up to date (release: 2017-Jun-06)
+* RouterOS is upgradable to 6.38.7 (release: 2017-Jun-20
 
 ## Configuration
 
@@ -18,18 +18,16 @@ This script checks if there is an upgrade for a mikrotik device available. It sh
 The default values for SNMP are:
 * PORT=161
 * COMMUNITY=public
-* RELEASE-TREE=stable 
 
 For a check over SSH:
 * PORT=22
 * USER=user
-* RELEASE-TREE=stable 
 
-You have to setup an user with ssh-key-access. There are no options for a password.
+You need an user and SSH keys. There is no option for a password.
 
 **[RELEASE TREE]**
 
-Here are different settings usable. Select yours:
+Different settings are available. The default is `stable`. Choose wise.
 
 * stable
 * bugfix
--- check_routeros-upgrade.sh
+++ check_routeros-upgrade.sh
@@ -1,48 +1,47 @@
 #!/bin/sh
 
-# check_routeros-upgrade
-# https://github.com/x70b1/check_routeros-upgrade
+#  check_routeros-upgrade.sh
 #
-# This script checks if there is an upgrade for a mikrotik device available.
+#  A simple monitoring plugin to check RouterOS for updates.
 
-ERROR_MSG="0"
+error="0"
 
-# connect to a device
+
 if [ "$1" = "snmp" ]; then
     if [ -z "$3" ]; then
-        PARAM_PORT="161"
+        param_port="161"
     else
-        PARAM_PORT=$3
+        param_port=$3
     fi
 
     if [ -z "$4" ]; then
-        PARAM_COMMUNITY="public"
+        param_community="public"
     else
-        PARAM_COMMUNITY=$4
+        param_community=$4
     fi
 
-    if ! ROUTEROS_INSTALLED=$(snmpget -O qv -v 2c -c "$PARAM_COMMUNITY" "$2":"$PARAM_PORT" SNMPv2-SMI::enterprises.14988.1.1.4.4.0 2> /dev/null); then
-        ERROR_MSG="Could not establish an SNMP connection to the device!"
+    if ! routeros_installed=$(snmpget -O qv -v 2c -c "$param_community" "$2":"$param_port" SNMPv2-SMI::enterprises.14988.1.1.4.4.0 2> /dev/null); then
+        error="Could not establish an SNMP connection to the device!"
     else
-        ROUTEROS_INSTALLED=$(echo "$ROUTEROS_INSTALLED" | tr -d '"')
+        routeros_installed=$(echo "$routeros_installed" | tr -d '"')
     fi
 elif [ "$1" = "ssh" ]; then
     if [ -z "$3" ]; then
-        PARAM_PORT="22"
+        param_port="22"
     else
-        PARAM_PORT=$3
+        param_port=$3
     fi
 
     if [ -z "$4" ]; then
-        PARAM_USER="user"
+        param_user="user"
     else
-        PARAM_USER=$4
+        param_user=$4
     fi
 
-    if ! ROUTEROS_INSTALLED=$(ssh -q -p "$PARAM_PORT" "$PARAM_USER"@"$2" ':put [/system package get system version]'); then
-        ERROR_MSG="Could not establish an SSH connection to the device!"
+    if ! routeros_installed=$(ssh -q -p "$param_port" "$param_user"@"$2" ':put [/system package get system version]'); then
+        error="Could not establish an SSH connection to the device!"
     else
-        ROUTEROS_INSTALLED=$(echo $ROUTEROS_INSTALLED | tr -d '\r\n')
+        routeros_installed=$(echo "$routeros_installed" | tr -d "\r\n")
     fi
 else
     echo "Use SNMP or SSH as connection type!"
@@ -50,43 +49,42 @@ else
 fi
 
 
-# are there connection errors?
-if [ "$ERROR_MSG" = "0" ]; then
-    # which version should run?
+if [ "$error" = "0" ]; then
+    # select the version
     case $5 in
         "stable")
-            PARAM_VERSION="6"
+            param_version="6"
             ;;
         "release-candidate")
-            PARAM_VERSION="6rc"
+            param_version="6rc"
             ;;
         "bugfix")
-            PARAM_VERSION="6fix"
+            param_version="6fix"
             ;;
         *)
-            PARAM_VERSION="6"
+            param_version="6"
             ;;
     esac
 
     # check the mikrotik server for upgrades
-    if ! ROUTEROS_AVAILABLE=$(curl -f -s -A 'check_routeros-upgrade' http://upgrade.mikrotik.com/routeros/LATEST.$PARAM_VERSION); then 
-        echo "Could not reach the mikrotik upgrade server :("
+    if ! routeros_available=$(curl -fsA 'check_routeros-upgrade' http://upgrade.mikrotik.com/routeros/LATEST.$param_version); then 
+        echo "Could't reach the Mikrotik-Server!"
         exit 1
 
     fi
 
-    ROUTEROS_AVAILABLE_VERSION=$(echo "$ROUTEROS_AVAILABLE" | cut -d ' ' -f 1)
-    ROUTEROS_AVAILABLE_RELEASEDATE=$(echo "$ROUTEROS_AVAILABLE" | cut -d ' ' -f 2)
+    routeros_available_version=$(echo "$routeros_available" | cut -d ' ' -f 1)
+    routeros_available_releasedate=$(echo "$routeros_available" | cut -d ' ' -f 2)
 
-    # check latest requested version against the device version
-    if [ "$ROUTEROS_AVAILABLE_VERSION" = "$ROUTEROS_INSTALLED" ]; then
-        echo "RouterOS version $ROUTEROS_AVAILABLE_VERSION is up to date (Release: $(date -d @"$ROUTEROS_AVAILABLE_RELEASEDATE" +'%a %d %b %Y'))"
+    # compare latest version with device version
+    if [ "$routeros_available_version" = "$routeros_installed" ]; then
+        echo "RouterOS $routeros_available_version is up to date (release: $(date -u -d @"$routeros_available_releasedate" +'%Y-%b-%d'))"
         exit 0
     else
-        echo "RouterOS is upgradable to version $ROUTEROS_AVAILABLE_VERSION (Release: $(date -d @"$ROUTEROS_AVAILABLE_RELEASEDATE" +'%a %d %b %Y'))"
+        echo "RouterOS is upgradable to $routeros_available_version (release: $(date -u -d @"$routeros_available_releasedate" +'%Y-%b-%d'))"
         exit 2
     fi
 else
-    echo "$ERROR_MSG"
+    echo "$error"
     exit 1
 fi
